ef047d083304503c760064bd0a56a79859f4d064
WilsonWilson <pete@mymettle.com>
2020-07-19 12:33:47 -0500

    manifest

---
+++ precache-manifest.56b3407dce8154e25ddfde53d2eacb00.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "721ded2f83f887c4d3ff3c40ba87f2a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c650d41b72f2def6435",
+    "url": "/style.css"
+  }
+  {
+    "revision": "6745acac77d7f9c9fb7b",
+    "url": "/script.js"
+  }
+]);
